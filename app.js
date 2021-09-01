a8436f2f85d1b60ed51c5f2c1a9f8f98414aa059
santheartist <vazquezbare@gmail.com>
2021-09-01 09:13:16 -0300

    first commit with npm

---
+++ app.js
@@ -0,0 +1,11 @@
+// Copy email to user's clipboard
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
+
+
+//Scroll effect
